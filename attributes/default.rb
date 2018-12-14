3c9e747c97104c964919bd5cc3f940a284d23a50
timothy wright <tim@eastbound.io>
2018-12-14 16:16:18 +0000

    initial commit

---
+++ attributes/default.rb
@@ -0,0 +1,10 @@
+
+
+# prevent a newer client "updating" to an older client
+default['chef_client_updater']['prevent_downgrade'] = true
+
+# the version to install (ex: '12.12.13') or 'latest'
+default['chef_client_updater']['version'] = '14.8.12'
+
+# kill the client post install or exec the client post install for non-service based installs
+default['chef_client_updater']['post_install_action'] = 'exec'
---
+++ recipes/default.rb
@@ -0,0 +1,1 @@
+include_recipe 'chef_client_updater::default'
