301f7738cd8af007b95f5592ad58c00e2dfb933c
hieunba <hi3unb@gmail.com>
2019-03-02 02:04:22 +0700

    Use chef-client 14.10.9

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,7 @@
 default['chef_client_updater']['prevent_downgrade'] = false
 
 # the version to install (ex: '12.12.13') or 'latest'
-default['chef_client_updater']['version'] = '14.8.12'
+default['chef_client_updater']['version'] = '14.10.9'
 
 # kill the client post install or exec the client post install for non-service based installs
 default['chef_client_updater']['post_install_action'] = 'exec'
